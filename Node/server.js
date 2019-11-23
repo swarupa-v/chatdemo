915facb0bfffec3d882f8f8129b777f39910d450
Swarupa Vengurlekar <Swarupa Vengurlekar>
2019-11-23 23:31:27 +0530

    Multiple namespace change

--- Node/server.js
+++ Node/server.js
@@ -42,6 +42,36 @@ const newLocal = 'chatmessage';
 
    }); 
  });
+
+ var nsp1 = io.of('/my-namespace1');
+
+ nsp1.on('connection', function(socket) {
+    console.log('someone connected1');
+    nsp1.emit('hi', registeredRooms.toString() );
+
+   console.log('join room');
+    socket.on("joinroom",room=>{
+        console.log("Joining Room...: " + room);
+        if (registeredRooms.includes(room)) {
+          //Socket has joined the request room
+          
+          socket.join(room);
+
+          io.of("/my-namespace1").in(room).emit("message", "New user has been connected to the " + room + " Room");
+          return socket.emit("roomresult", "Valid Room Name: " + room);
+        } else {
+         //No room with the specified Name! (or it could be another reason).
+         return socket.emit("roomerr", "Invalid Room Name: " + room);
+        }        
+    });
+
+    console.log('chat message');
+    socket.on('chatmessage', function (data) {
+      console.log('chatmessage',data);
+      io.of('/my-namespace1').to(data.room).emit('messagebroadcast', data.message);
+
+   }); 
+ });
  
  http.listen(3000, function() {
     console.log('listening on localhost:3000');
